00498194ef854186b64dffef6f6cb9d566de14c4
alejandrosalgar <alejandro.salgar@gmail.com>
2026-02-11 19:11:13 -0500

    feat: agregar cantidad

--- clases/ropa.py
+++ clases/ropa.py
@@ -1,7 +1,8 @@
 class Ropa:
-    def __init__(self, color: str, marca: str):
+    def __init__(self, color: str, marca: str, cantidad: int):
         self.color = color
         self.marca = marca
+        self.cantidad = cantidad
 
     def imprimir_datos(self) -> None:
         print(f"el color de la ropa es {self.color} y la marca es {self.marca}")
