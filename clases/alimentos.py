049dd6855c888469248a109d4e52a247214c8a0a
Laura-Hernández-S <122laurasofia@gmail.com>
2026-02-26 21:32:51 -0500

    feat: Clases de alimentos, electronicos y ropa

--- clases/alimentos.py
+++ clases/alimentos.py
@@ -1,1 +1,10 @@
-# alimentos
+class Alimentos:
+    def __init__(self, tipo_alimento: str, nombre: str, cantidad: int):
+        self.tipo_alimento = tipo_alimento
+        self.nombre = nombre
+        self.cantidad = cantidad
+
+    def imprimir_datos(self) -> None:
+        print(f"Tipo de alimento: {self.tipo_alimento}")
+        print(f"Nombre: {self.nombre}")
+        print(f"Cantidad: {self.cantidad}")
